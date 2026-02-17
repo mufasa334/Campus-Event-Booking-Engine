fb8527f67e35cea0866dabaee5de7c04344b287c
Jason123451208 <jvieir06@uoguelph.ca>
2026-02-17 15:56:55 -0500

    Added more to the constructor, with more necessary information needed to run the BookingManger class. And added some of the needed methods to the class (add a user, remove a user, and print attendance list (booked and waitlisted)).

--- src/main/java/org/example/BookingManager.java
+++ src/main/java/org/example/BookingManager.java
@@ -3,24 +3,33 @@ import java.util.ArrayList;
 
 public class BookingManager {
     public ArrayList<String> UserList = new ArrayList<>();
-    public int numberInList;
-    public String enteredID;
     public int capacity;
 
     private String UserID;
+    private String UserName;
     private String EventID;
+    private String EventName;
     private String BookingID;
     private String status;
 
     //getter and setters
 
-    public String getUserID(int numberInList) {
+    //the UserID stuff seems unnecessary if all that is really needed is the Username
+    public String getUserID(int numberInList){
         UserID = UserList.get(numberInList);
+        //would need some conversion factor here to check what the userID goes with what username, and set the new UserID to that
         return UserID;
     }
-
     public void setUserID(String UserID){
-       this.UserID = UserID;
+        this.UserID = UserID;
+    }
+    public String getUserName(int numberInList) {
+        UserName = UserList.get(numberInList);
+        return UserName;
+    }
+
+    public void setUserName(String UserName){
+       this.UserName = UserName;
     }
 
     public String getEventID(){
@@ -31,6 +40,14 @@ public class BookingManager {
         this.EventID = EventID;
     }
 
+    public String getEventName(){
+        return EventName;
+    }
+
+    public void setEventName(String EventName){
+        this.EventName = EventName;
+    }
+
     public String getBookingID(){
         return BookingID;
     }
@@ -39,9 +56,9 @@ public class BookingManager {
         this.BookingID = BookingID;
     }
 
-    public String getStatus(String enteredID){
+    public String getStatus(String enteredName){
         for (int i = 0; i < capacity; i++) {
-            if (enteredID.equals(UserList.get(i))){
+            if (enteredName.equals(UserList.get(i))){
                 status = "Confirmed";
                 return status;
 
@@ -52,7 +69,7 @@ public class BookingManager {
             }
         }
         for (int j = capacity; j < (j+1); j++){
-            if (enteredID.equals(UserList.get(j))){
+            if (enteredName.equals(UserList.get(j))){
                 status = "WaitListed";
                 return status;
 
@@ -73,11 +90,71 @@ public class BookingManager {
 
 
     //constructor
-    public BookingManager (int capacity){
+    public BookingManager (int capacity, String EventID, String EventName){
         this.capacity = capacity;
+        this.EventID = EventID;
+        this.EventName = EventName;
+        UserList.add(null);
     }
 
     //methods
+    public void addUser(String enteredUserName){
+        for (int i = 0; i < (i+1); i++) {
+            if (UserList.get(i) == null){
+                UserList.set(i, enteredUserName);
+                break;
+            }
+            if(enteredUserName.equals(UserList.get(i))) {
+                System.out.println("User Is Already Listed"); //would need to print to UI
+                break;
+            }
+        }
+        UserList.add(null);
 
+    }
+
+    public void cancelBooking(String enteredUserName){
+        for (int i = 0; i < capacity; i++) {
+            if (enteredUserName.equals(UserList.get(i))){
+                UserList.remove(i);
+            }
+            if(UserList.get(i) == null){
+                break;
+            }
+        }
+        for (int j = capacity; j < (j+1); j++){
+            if (enteredUserName.equals(UserList.get(j))){
+                UserList.remove(j);
+            }
+            if (UserList.get(j) == null){
+                break;
+
+            }
+        }
+    }
+
+    //note that a lot of the prints in this part will need to be printed/shown in the UI later not in the terminal
+    public void bookWaitlistPrint(){
+        System.out.println("Event Name/ID: " + EventName + "/" + EventID);
+        System.out.println("Users Booked: ");
+        for (int i = 0; i < capacity; i++) {
+            if (UserList.get(i) != null){
+                System.out.println(UserList.get(i));
+            }
+            if(UserList.get(i) == null){
+                break;
+            }
+        }
+        System.out.println("Users Waitlisted: ");
+        for (int j = capacity; j < (j+1); j++){
+            if (UserList.get(j) != null){
+                System.out.println(UserList.get(j));
+            }
+            if (UserList.get(j) == null){
+                break;
+
+            }
+        }
+    }
 
 }
